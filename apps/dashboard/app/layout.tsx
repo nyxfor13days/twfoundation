9f0c9910005113fdc0f6ed1228d1f453a6a35af7
nyxfor13days <nyxfor13days@gmail.com>
2023-01-07 16:55:10 +0530

    Added default styles in globals.scss

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { LayoutProps } from '../types/Layout';
-import './globals.scss';
+import type { LayoutProps } from "../types/Layout";
+import "./stylesheets/globals.scss";
 
 const RootLayout = ({ children }: LayoutProps) => {
-	return (
-		<html lang='en'>
-			<head />
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
 };
 
 export default RootLayout;

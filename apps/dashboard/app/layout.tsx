640e755ee3cd92c01b2a82649ed8d799a1aa93a2
nyxfor13days <nyxfor13days@gmail.com>
2023-01-07 14:46:15 +0530

    Created Dashboard App

---
+++ apps/dashboard/app/layout.tsx
@@ -0,0 +1,13 @@
+import type { LayoutProps } from '../types/Layout';
+import './globals.scss';
+
+const RootLayout = ({ children }: LayoutProps) => {
+	return (
+		<html lang='en'>
+			<head />
+			<body>{children}</body>
+		</html>
+	);
+};
+
+export default RootLayout;
